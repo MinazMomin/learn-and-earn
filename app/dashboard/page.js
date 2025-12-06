2f59210f492da4355a4c19007bf0d5f3836fccfe
Minaz <mminazmomin02@gmail.comgit config --giobal --listgit config --global user.email>
2025-12-05 22:21:36 -0600

    Fix firebase path properly

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { auth } from "../../firebase";
+import { auth } from "@/firebase/firebase";
 import { signOut } from "firebase/auth";
 import { useRouter } from "next/navigation";
 
